5475c1f9f3dc7a844070245e5db1543808c87c8f
Giventus <weishen735@gmail.com>
2024-04-25 11:41:35 +0700

    Initial Commit

---
+++ testttt/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  testttt
+//
+//  Created by Giventus Marco Victorio Handojo on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var expandedIndexes: Set<Int> = []
+    
+    let accordionData = [
+        ("Accordion 1 Title", "Accordion 1 Content"),
+        ("Accordion 2 Title", "Accordion 2 Content"),
+        ("Accordion 3 Title", "Accordion 3 Content"),
+        ("Accordion 4 Title", "Accordion 4 Content"),
+        ("Accordion 5 Title", "Accordion 5 Content")
+    ]
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<accordionData.count, id: \.self) { index in
+                AccordionView(title: self.accordionData[index].0,
+                              content: self.accordionData[index].1,
+                              isExpanded: self.expandedIndexes.contains(index),
+                              onToggle: {
+                                  withAnimation {
+                                      if self.expandedIndexes.contains(index) {
+                                          self.expandedIndexes.remove(index)
+                                      } else {
+                                          self.expandedIndexes.insert(index)
+                                      }
+                                  }
+                              })
+            }
+        }
+    }
+}
+
+struct AccordionView: View {
+    var title: String
+    var content: String
+    var isExpanded: Bool
+    var onToggle: () -> Void
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.onToggle()
+            }) {
+                HStack {
+                    Text(title)
+                    Spacer()
+                    Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+                }
+                .padding()
+            }
+            if isExpanded {
+                Text(content)
+                    .padding()
+            }
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(8)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testttt/testtttApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtttApp.swift
+//  testttt
+//
+//  Created by Giventus Marco Victorio Handojo on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct testtttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
